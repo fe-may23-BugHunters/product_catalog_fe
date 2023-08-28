f8d9699047e4a078d55e17ae0b58651021ed402f
Valentyna <vabo1108@gmail.com>
2023-08-28 21:50:20 +0200

    add header adaptive layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Logo } from '../Logo';
+import { Navigation } from '../Navigation';
+
+import './Header.scss'
+
+export const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header__left">
+        <Logo />
+
+        <Navigation />
+      </div>
+    </header>
+  )
+}
+
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,36 @@
+import './Navigation.scss'
+import React from 'react'
+
+export const Navigation: React.FC = () => {
+  return (
+    <nav className="nav">
+    <ul className="nav__list">
+      <li className="nav__item">
+        <a className="nav__link" href="home"
+        >
+          home
+        </a>
+      </li>
+      <li className="nav__item">
+        <a className="nav__link" href="phones"
+        >
+          phones
+        </a>
+      </li>
+      <li className="nav__item">
+        <a className="nav__link" href="tablets"
+        >
+          tablets
+        </a>
+      </li>
+      <li className="nav__item">
+        <a className="nav__link" href="accessories"
+        >
+          accessories
+        </a>
+      </li>
+    </ul>
+  </nav>
+  )
+}
+
