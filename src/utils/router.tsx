efbbd2d855cae3892bb274e753c5fff49d8de4b8
tetianakostiuk <ipz213_kto@student.ztu.edu.ua>
2023-08-29 15:51:21 +0300

    added App Router

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,1 +1,30 @@
-export const router = '';
+import React, { FC } from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+// import { HomePage } from '../pages/HomePage';
+// import { CatalogPage } from '../pages/CatalogPage';
+// import { ItemPage } from '../pages/ItemPage';
+// import { FavouritesPage } from '../pages/FavouritesPage';
+// import { CartPage } from '../pages/CartPage';
+// import { NotFoundPage } from '../pages/NotFoundPage';
+
+
+export const AppRouter: FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/product_catalog_fe">
+          <Route index element={<h1>Home Page</h1>} />
+          <Route path="catalog">
+            <Route index element={<h1>catalog Page</h1>} />
+            <Route path=":itemId?" element={<h1>item Page</h1>} />
+          </Route>
+
+          <Route path="favourites" element={<h1>favourites Page</h1>} />
+          <Route path="cart" element={<h1>cart Page</h1>} />
+        </Route>
+
+        <Route path="*" element={<h1>Not Found Page</h1>} />
+      </Routes>
+    </Router>
+  );
+};
