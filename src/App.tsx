9de4eb95fb90237244f980c86dd57ad7577d2375
Valentyna <vabo1108@gmail.com>
2023-08-30 22:10:46 +0200

    fixed not-found-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './styles/main.scss';
 import { MainLayout } from './layouts/MainLayout';
-import { Example } from './components/Example';
 import { NotFound } from './components/NotFound';
 import { Outlet } from 'react-router-dom';
 
@@ -9,7 +8,6 @@ const App: React.FC = () => {
   return (
     <MainLayout>
       <NotFound />
-      <Example />
       <Outlet />
     </MainLayout>
   );
--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 
 export const NotFound: React.FC = () => {
   return (
-    <div id='main'>
-      <div className='notfound'>
-        <h1 className='notfound__title'>404 - Page Not Found</h1>
-
-        <p className='notfound__title'>
-          Oops! The page you are looking for does not exist
-        </p>
-      </div>
-    </div>
+    <div
+      className="not-found"
+    >
+    <h1
+      className="not-found__title"
+    >
+      404 - Page Not Found
+    </h1>
+    <p
+      className="not-found__message"
+    >
+      Oops! The page you are looking for does not exist
+    </p>
+  </div>
   );
 };
