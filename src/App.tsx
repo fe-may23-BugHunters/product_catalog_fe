f2234b3524805914a37ec5a924825898a5e7d96b
Valentyna <vabo1108@gmail.com>
2023-08-31 15:14:57 +0200

    changed not-found-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './styles/main.scss';
 import { MainLayout } from './layouts/MainLayout';
-import { NotFound } from './components/NotFound';
 import { Outlet } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <MainLayout>
-      <NotFound />
       <Outlet />
     </MainLayout>
   );
