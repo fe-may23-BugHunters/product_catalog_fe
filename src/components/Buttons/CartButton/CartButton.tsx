586c9602268465298e02aa2e34bcedd13645d235
Kateryna Kylyvnyk <kkylyvnyk@gmail.com>
2023-09-02 10:26:17 +0300

    add React.FC to CartButton

--- src/components/Buttons/CartButton/CartButton.tsx
+++ src/components/Buttons/CartButton/CartButton.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import cn from 'classnames';
 import './CartButton.scss';
 
-export const CartButton = () => {
+export const CartButton: React.FC = () => {
   const [isAddedToCart, setIsAddedToCart] = useState(false);
 
   return (
