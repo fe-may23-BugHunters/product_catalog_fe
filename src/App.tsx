c6a3ce72d68f82880dede864e6fcd13bc81433ab
Anatolii Lykhohodenko <anatollykh10@gmail.com>
2023-09-03 21:44:32 +0300

    add basic logic api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/main.scss';
 import { MainLayout } from './layouts/MainLayout';
 import { Outlet } from 'react-router-dom';
+import { getProductById, getSpecifiedProducts } from './api/fetchClient';
 
 const App: React.FC = () => {
+  const [products, setProducts] = useState<any>([]);
+  const [product, setProduct] = useState<any>({});
+
+  useEffect(() => {
+    getSpecifiedProducts()
+    .then(products => {
+      setProducts(products.data);
+      console.log(products);
+    })
+    .catch(err => {
+      console.log(err);
+
+      return Promise.reject();
+    });
+
+    getProductById('e50752f4-46a4-11ee-ac61-fed709f87b70')
+    .then(product => {
+      setProduct(product.data);
+      console.log(product);
+    })
+    .catch(err => {
+      console.log(err);
+
+      return Promise.reject();
+    });
+  }, []);
+
   return (
-    <MainLayout>
-      <Outlet />
-    </MainLayout>
+    <>
+      {JSON.stringify(products.rows)}
+      <img src={`product_catalog_fe/${product.image}`}></img>
+      <MainLayout>
+        <Outlet />
+      </MainLayout>
+    </>
   );
 };
 
