a1a8fb80972cd275083df56b6da712139f458a29
Kateryna Kylyvnyk <kkylyvnyk@gmail.com>
2023-09-04 20:57:09 +0300

    fix

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -10,43 +10,37 @@ export const Categories: React.FC = () => {
     <section className="categories">
       <div className="categories__products">
         <article className="categories__card">
-          <NavLink to="/phones">
+          <NavLink to="/phones" className="categories__photo">
             <img
               src={phones}
               alt="category_phones"
               className="categories__photo"
             />
           </NavLink>
-          <h3 className="categories__title">
-            Mobile phones
-          </h3>
+          <h3 className="categories__title">Mobile phones</h3>
           <p className="categories__amount">95 models</p>
         </article>
         <article className="categories__card">
-          <NavLink to="/phones">
+          <NavLink to="/tablets" className="categories__photo">
             <img
               src={tablets}
               alt="category_phones"
               className="categories__photo"
             />
           </NavLink>
-          <h3 className="categories__title">
-            Tablets
-          </h3>
+          <h3 className="categories__title">Tablets</h3>
           <p className="categories__amount">24 models</p>
         </article>
 
         <article className="categories__card">
-          <NavLink to="/accessories">
+          <NavLink to="/accessories" className="categories__photo">
             <img
               src={accessories}
               alt="category_accessories"
               className="categories__photo"
             />
           </NavLink>
-          <h3 className="categories__title">
-            Accessories
-          </h3>
+          <h3 className="categories__title">Accessories</h3>
           <p className="categories__amount">100 models</p>
         </article>
       </div>
