9aa17d9251dfa829702fb3e424e18fe10be1cc13
Valentyna <vabo1108@gmail.com>
2023-09-06 00:30:59 +0200

    fixed BurgerMenu bug

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -14,6 +14,7 @@ export const Buttons: React.FC = () => {
   useEffect(() => {
     const body = document.querySelector('body') as HTMLBodyElement;
 
+    body.style.height = isActiveBurger ? '100vh' : 'auto';
     body.style.overflow = isActiveBurger ? 'hidden' : 'visible';
   }, [isActiveBurger]);
 
