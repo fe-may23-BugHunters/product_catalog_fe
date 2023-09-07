b7e1b220b6a65e7ea9b1865899d087593e0e19e2
IlonaBorkovska <borkovska.io@gmail.com>
2023-09-07 16:00:16 +0300

    add rights contacts layouts

---
+++ src/pages/RightsPage/RightsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RightsPage.scss';
+
+export const RightsPage: React.FC = () => {
+  return (
+    <div className="rights">
+      <h1 className="rights__title">Rights</h1>
+
+      <p className="rights__message">There is no rights yet &copy;2023</p>
+    </div>
+  );
+};
